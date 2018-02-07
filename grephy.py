1e6efb4bad01c832251042ee3a65605a84e0dc11
Jesse-Opitz <jesse.opitz1@marist.edu>
2018-02-07 13:35:52 -0500

    Grabs alphabet from file, added logging

--- grephy.py
+++ grephy.py
@@ -16,21 +16,51 @@ Description:
 Due Date: 05/07/18
 '''
 
-import argparse
+import argparse, logging, sys
 
+def read_file(fname):
+    try:
+        with open(fname, 'r') as f:
+            data = f.read();
+
+        return data
+    except IOError as e:
+        logging.critical("FILE['{2}'] I/O error({0}): {1}".format(e.errno, e.strerror, fname))
+        sys.exit(1)
+    except:
+        logging.critical("Unexpected error:", sys.exc_info()[0])
+        raise
+        sys.exit(1)
+
+def get_alphabet(fname):
+    alphabet = ''
+    data = read_file(fname)
+    for line in data:
+        for c in line:
+            if c not in alphabet:
+                alphabet += c
+
+    logging.debug("Alphabet: " + repr(alphabet))
+
+    return alphabet
 
 def main():
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
+
     parser = argparse.ArgumentParser(description='Searches files for regular expression pattern matches.')
     
     parser.add_argument('-n', '--NFA-FILE', nargs=1, help='Output file for NFA')
     
     parser.add_argument('-d', '--DFA-FILE', nargs=1, help='Output file for DFA')
 
-    parser.add_argument('REGEX', type=str, help='Regular expression file')
+    #parser.add_argument('REGEX', type=str, help='Regular expression file')
     
     parser.add_argument('FILE', type=str, help='Input file')
 
     args = parser.parse_args()
+
+    alphabet = get_alphabet(args.FILE)
+
 
 if __name__ == "__main__":
     main()
