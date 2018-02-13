059d506abb58694dfcadf69e4fcf8366a8d90d3e
Jesse-Opitz <jesse.opitz1@marist.edu>
2018-02-12 21:37:00 -0500

    Added self to params, removed comments

--- finite_automata.py
+++ finite_automata.py
@@ -16,22 +16,17 @@ class finite_automata:
     Object containing 5 tuple needed for creating an NFA or DFA.
     
     @type   states:      list of type state
-    @param  states:      All available states of NFA/DFA
     @type   alphabet:    string
-    @param  alphabet:    Alphabet of the NFA/DFA
     @type   transitions: list of type edge
-    @param  transitions: (delta) All transitions of the NFA/DFA
     @type   init_state:  string
-    @param  init_state:  Initial state of the NFA/DFA
     @type   acc_state:   list of type state
-    @param  acc_state:   Accepting states of the NFA/DFA
     """
-    def __init__(self, alphabet):
+    def __init__(self):
         # List of states
         self.states = []
         
         # Alphabet
-        self.alphabet = alphabet
+        self.alphabet = ''
         
         # List of transitions
         self.transitions = []
@@ -45,56 +40,59 @@ class finite_automata:
     def draw(outfile):
         print 'draw finite automata'
     
-    def add_state(name, is_init, is_acc):
+    def set_alphabet(self, ab):
+        self.alphabet = ab
+
+    def get_alphabet(self):
+        # returns string of alphabet
+        return repr(self.alphabet)
+
+    def add_state(self, name, is_init, is_acc):
         new_state = State(name, is_init, is_acc)
         self.states.append(new_state)
 
-    def rem_state(state_name):
+    def rem_state(self, state_name):
         print 'remove', state_name
     
-    def add_transition(name, source, target):
+    def add_transition(self, name, source, target):
         new_edge = Edge(name, source, target)
         self.transitions.append(new_edge)
 
-    def rem_transition(edge_name):
+    def rem_transition(self, edge_name):
         print 'remove', edge_name
 
-    def add_acc_state(state):
+    def add_acc_state(self, state):
         # go through list of states
         # change state status is_acc = True
         # add to list of acc_states
         self.acc_states.append(state)
     
-    def rem_acc_state(state):
+    def rem_acc_state(self, state):
         # go through list of states
         # change state status is_acc = False
         # rem from list of acc_states
         print 'remove', state
     
-    def get_states():
+    def get_states(self):
         # returns a string of states
         return ''
 
-    def get_alphabet():
-        # returns string of alphabet
-        return repr(self.alphabet)
-
-    def get_transitions():
+    def get_transitions(self):
         # returns list of transitions
         return []
 
-    def get_init():
+    def get_init(self):
         # returns initial state
         return self.init_state
 
-    def get_acc_state():
+    def get_acc_state(self):
         # returns list of accepting states
         return []
 
-    def print_transitions():
+    def print_transitions(self):
         # returns a string of a list of transitions
         return ''
 
-    def print_acc_states():
+    def print_acc_states(self):
         # returns a string of a list of accepting states
         return ''
