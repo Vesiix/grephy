c20bbec86f74726da0be2757e00fb0b3722fc82b
Jesse-Opitz <jesse.opitz1@marist.edu>
2018-02-21 01:56:51 -0500

    Started a file for drawing nfa/dfa in dot format

---
+++ dot_draw_fa.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+dot_draw_fa.py
+
+Description:
+    Draws a finite automata given a NFA/DFA object in a dot format.
+
+@author: Jesse Opitz
+
+"""
+import graphviz
+
+def draw_init(state):
+    print 'draw_init'    
+
+def dot_draw(fa):
+    print 'dot_draw'
