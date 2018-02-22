b1e9502caf035fb48fed14c412662c918581aee3
Jesse-Opitz <jesse.opitz1@marist.edu>
2018-02-22 04:11:48 -0500

    Added simple zero or more regex(*) function that works

--- create_nfa.py
+++ create_nfa.py
@@ -30,6 +30,7 @@ def create_nfa(ab, rgx):
 
     return nfa
 
+# rgx: <string>
 def simple_rgx(rgx, nfa):
     p = 0
 
@@ -53,13 +54,54 @@ def simple_rgx(rgx, nfa):
         nfa.add_transition(rgx[p], prev_state, nfa.get_state('q' + str(nfa.get_next_state()-1)))
         p += 1
 
-def parse_rgx(rgx, nfa):
-    if re.match('^[a-zA-Z0-9]*$', rgx):
-        simple_rgx(rgx, nfa)
-    else:
-        # simple regex with |'s
-        if re.match('^([a-zA-Z0-9]*\|[a-zA-Z0-9]*)*$', rgx):
-            split_rgx = rgx.split('|')
-            for s in split_rgx:
-                simple_rgx(s, nfa)
+# rgx: <string>*
+def zero_or_more_rgx(rgx, nfa):
+    p = 0
+    prev_state = nfa.get_state('q0')
+    
+    nfa.add_state('q' + str(nfa.get_next_state()), False, False)
+    nfa.add_transition(u'\u0395', prev_state, nfa.get_state('q' + str(nfa.get_next_state()-1)))
+    
+    loop_state = nfa.get_state('q' + str(nfa.get_next_state()-1))
 
+    while rgx[p] != '*':
+        prev_state = nfa.get_state('q' + str(nfa.get_next_state()-1))
+        nfa.add_state('q' + str(nfa.get_next_state()), False, False)
+        nfa.add_transition(rgx[p], prev_state, nfa.get_state('q' + str(nfa.get_next_state()-1)))
+        p += 1
+
+    prev_state = nfa.get_state('q' + str(nfa.get_next_state()-1))
+
+    nfa.add_state('q' + str(nfa.get_next_state()), False, True)
+    nfa.add_transition(u'\u0395', prev_state, nfa.get_state('q' + str(nfa.get_next_state()-1)))
+    nfa.add_transition(u'\u0395', nfa.get_state('q' + str(nfa.get_next_state()-1)), loop_state)
+    nfa.add_transition(u'\u0395', nfa.get_state('q0'), nfa.get_state('q' + str(nfa.get_next_state()-1)))
+    
+    
+def parse_rgx(rgx, nfa):
+    """
+    c = 0
+    while c < len(rgx):
+        if re.match('[a-zA-Z0-9\.]', rgx[c]):
+            if c < len(rgx)-1:
+                if re.match('[\*\+{\?]', rgx[c+1]):
+                    if rgx[c+1] == '*':
+                        zero_or_more_rgx(rgx, nfa)
+
+        
+        c += 1
+    """
+    # Simple regex, no symbols 
+    if re.match('^[a-zA-Z0-9\.]*$', rgx):
+        simple_rgx(rgx, nfa)
+    # Or symbol
+    elif re.match('^([a-zA-Z0-9\.]*\|[a-zA-Z0-9\.]*)*$', rgx):
+        # Only takes in simple rgx
+        split_rgx = rgx.split('|')
+        for s in split_rgx:
+            simple_rgx(s, nfa)
+    # Zero or more (*)
+    elif re.match('^([a-zA-Z0-9\.]*\*[a-zA-Z0-9\.]*)*$$', rgx):
+        # if there's a * somewhere
+        zero_or_more_rgx(rgx, nfa)
+    
