165117863db7922f3ebbc1dd07cc1028004244a5
Jesse-Opitz <jesse.opitz1@marist.edu>
2018-03-31 21:36:41 -0400

    Created skeleton for parsing regex

--- create_nfa.py
+++ create_nfa.py
@@ -20,7 +20,7 @@ def create_nfa(rgx):
 
     nfa.add_state('q0', True, False)
 
-    parse_rgx(rgx, nfa)
+    RE(rgx, nfa)
     
     logging.debug("NFA:")
     
@@ -30,36 +30,115 @@ def create_nfa(rgx):
 
     return nfa
 
-def simple_rgx(rgx, nfa):
-    p = 0
+# Removes the first letter of a string
+def next(s):
+    return s[1:]
 
-    if p == len(rgx)-1:
-        nfa.add_state('q' + str(nfa.get_next_state()), False, True)
+def union_RE(rgx, nfa):
+    RE(rgx, nfa)
+    if rgx[0] == "|":
+        # Create transition/nodes
+        rgx = next(rgx)
+        simple_RE(rgx, nfa)
+
+def simple_RE(rgx, nfa):
+    simple_first_set = re.compile("[\*\+\(\.\$a-zA-Z0-9\\\[") 
+
+    #if rgx[p] in simple_first_set:
+    if simple_first_set.match(rgx[0]):
+        basic_RE(rgx, nfa)
     else:
-        nfa.add_state('q' + str(nfa.get_next_state()), False, False)
+        concat_RE(rgx, nfa)
 
-    nfa.add_transition(rgx[p], nfa.get_state('q0'), nfa.get_state('q' + str(nfa.get_next_state()-1)))
-    
-    p += 1
+def concat_RE(rgx, nfa):
+    simple_RE(rgx, nfa)
+    basic_RE(rgx, nfa)
 
-    while p < len(rgx):
-        prev_state = nfa.get_state('q' + str(nfa.get_next_state()-1))
+def basic_RE(rgx, nfa):
+    star_RE(rgx, nfa)
+    plus_RE(rgx, nfa)
+    elementary_RE(rgx, nfa)
 
-        if p == len(rgx)-1:
-            nfa.add_state('q' + str(nfa.get_next_state()), False, True)
-        else:
-            nfa.add_state('q' + str(nfa.get_next_state()), False, False)
+def star_RE(rgx, nfa):
+    elementary_RE(rgx, nfa)
+    if rgx[0] == "*":
+        rgx = next(rgx)
 
-        nfa.add_transition(rgx[p], prev_state, nfa.get_state('q' + str(nfa.get_next_state()-1)))
-        p += 1
+def plus_RE(rgx, nfa):
+    elementary_RE(rgx, nfa)
+    if rgx[0] == "+":
+        rgx = next(rgx)
 
-def parse_rgx(rgx, nfa):
+def elementary_RE(rgx, nfa):
+    group_RE(rgx, nfa)
+    any_RE(rgx, nfa)
+    eos_RE(rgx, nfa)
+    char_RE(rgx, nfa)
+    set_RE(rgx, nfa)
+
+def group_RE(rgx, nfa):
+    if rgx[0] == "(":
+        rgx = next(rgx)
+        RE(rgx, nfa)
+        if rgx[0] == ")":
+            rgx = next(rgx)
+
+def any_RE(rgx, nfa):
+    if rgx[0] == ".":
+        rgx = next(rgx)
+
+def eos_RE(rgx, nfa):
+    if rgx[0] == "$":
+        rgx = next(rgx)
+
+def char_RE(rgx, nfa):
+    char_patt = re.compile("[a-zA-Z0-9]")
+    if char_patt.match(rgx[0]):
+        rgx = next(rgx)
+
+def set_RE(rgx, nfa):
+    pos_set_RE(rgx, nfa)
+    neg_set_RE(rgx, nfa)
+
+def pos_set_RE(rgx, nfa):
+    if rgx[0] == "[":
+        rgx = next(rgx)
+        set_items_RE(rgx, nfa)
+        if rgx[0] == "]":
+            rgx = next(rgx)
+
+def neg_set_RE(rgx, nfa):
+    if rgx[0] == "[":
+        rgx = next(rgx)
+        if rgx[0] == "^":
+            rgx = next(rgx)
+            set_items_RE(rgx, nfa)
+            if rgx[0] == "]":
+                rgx = next(rgx)
+
+def set_items_RE(rgx, nfa):
+    set_item_RE(rgx, nfa)
+
+    set_item_RE(rgx, nfa)
+    set_items_RE(rgx, nfa)
+
+def set_item_RE(rgx, nfa):
+    range_RE(rgx, nfa)
+    char_RE(rgx, nfa)
+
+def range_RE(rgx, nfa):
+    char_RE(rgx, nfa)
+    if rgx[0] == "-":
+        rgx = next(rgx)
+        char_RE(rgx, nfa)
+
+# Regex parser
+def RE(rgx, nfa):
     if re.match('^[a-zA-Z0-9]*$', rgx):
-        simple_rgx(rgx, nfa)
+        simple_RE(rgx, nfa)
     else:
+        union_RE(rgx, nfa)
         # simple regex with |'s
-        if re.match('^([a-zA-Z0-9]*\|[a-zA-Z0-9]*)*$', rgx):
-            split_rgx = rgx.split('|')
-            for s in split_rgx:
-                simple_rgx(s, nfa)
-
+        #if re.match('^([a-zA-Z0-9]*\|[a-zA-Z0-9]*)*$', rgx):
+        #    split_rgx = rgx.split('|')
+        #    for s in split_rg        #        simple_rgx(s, nfa)
