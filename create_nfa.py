116a7c49929c5a42c47674341ade4dbb085a45b9
Jesse-Opitz <jesse.opitz1@marist.edu>
2018-04-02 19:47:13 -0400

    Added curr_state to parse functions

--- create_nfa.py
+++ create_nfa.py
@@ -20,7 +20,8 @@ def create_nfa(rgx):
     nfa.add_state('q0', True, False)
     logging.debug("create_nfa")
 
-    rgx, nfa = RE(rgx, nfa)
+    curr_state = 0
+    rgx, nfa = RE(rgx, nfa, curr_state)
     
     if len(rgx) == 0:
         logging.debug("Parse completed successfully.")
@@ -39,104 +40,121 @@ def create_nfa(rgx):
 def next(s):
     return s[1:]
 
-def union_RE(rgx, nfa):
+def union_RE(rgx, nfa, curr_state):
     logging.debug("In union:" + rgx)
-    rgx, nfa = RE(rgx, nfa)
-    if len(rgx) > 0:
-        if rgx[0] == "|":
+    list_rgx = rgx.split('|')
+    first_rgx, nfa = RE(list_rgx[0], nfa, curr_state)
+    if len(first_rgx) > 0:
+        if len(list_rgx) > 1:
+            r = 1
+            while r < (len(list_rgx)-1):
+                curr_rgx, nfa = RE(list_rgx[r], nfa, curr_state)
+                r += 1
+                if len(curr_rgx) > 0:
+                    logging.critical("Error while parsing a part of union.")
+                    sys.exit(1)
+    else:
+        logging.critical("Error while parsing first part of union.")
+        sys.exit(1)
+
+    #if len(rgx) > 0:
+    #    if rgx[0] == "|":
             # Create transition/nodes
-            rgx = next(rgx)
-            rgx, nfa = simple_RE(rgx, nfa)
-        else:
-            logging.critical("Error in union parse.")
+    #        rgx = next(rgx)
+    #        rgx, nfa = simple_RE(rgx, nfa, curr_state)
+    #    else:
+    #        logging.critical("Error in union parse.")
+    #        sys.exit(1)
 
     logging.debug("Out union:" + rgx)
 
     return rgx, nfa
 
-def simple_RE(rgx, nfa):
+def simple_RE(rgx, nfa, curr_state):
     logging.debug("In simple:" + rgx)
     simple_first_set = re.compile("[\*\+\.\(\$\[\\a-zA-Z0-9]")
     # HOW DO YOU DIFFERENTIATE THESE???
     if len(rgx) > 0:
         if simple_first_set.match(rgx[0]):
-            rgx, nfa = basic_RE(rgx, nfa)
+            rgx, nfa = basic_RE(rgx, nfa, curr_state)
         else:
-            rgx, nfa = concat_RE(rgx, nfa)
+            rgx, nfa = concat_RE(rgx, nfa, curr_state)
  
     logging.debug("Out simple:" + rgx)
 
     return rgx, nfa
 
-def concat_RE(rgx, nfa):
+def concat_RE(rgx, nfa, curr_state):
     logging.debug("In concat:" + rgx)
-    rgx, nfa = simple_RE(rgx, nfa)
-    rgx, nfa = basic_RE(rgx, nfa)
+    rgx, nfa = simple_RE(rgx, nfa, curr_state)
+    rgx, nfa = basic_RE(rgx, nfa, curr_state)
     
     logging.debug("Out concat:" + rgx)
 
     return rgx, nfa
 
-def basic_RE(rgx, nfa):
+def basic_RE(rgx, nfa, curr_state):
     logging.debug("In basic:" + rgx)
-    rgx, nfa = elementary_RE(rgx, nfa)
+    rgx, nfa = elementary_RE(rgx, nfa, curr_state)
     if len(rgx) > 0:
         if rgx[0] == "*":
-            rgx, nfa = star_RE(rgx, nfa)
+            rgx, nfa = star_RE(rgx, nfa, curr_state)
         elif rgx[0] == "+":
-            rgx, nfa = plus_RE(rgx, nfa)
+            rgx, nfa = plus_RE(rgx, nfa, curr_state)
     logging.debug("Out basic:" + rgx)
 
     return rgx, nfa
 
-def star_RE(rgx, nfa):
+def star_RE(rgx, nfa, curr_state):
     logging.debug("In star:" + rgx)
     rgx = next(rgx)
     logging.debug("Out star:" + rgx)
 
     return rgx, nfa
 
-def plus_RE(rgx, nfa):
+def plus_RE(rgx, nfa, curr_state):
     logging.debug("In plus:" + rgx)
     rgx = next(rgx)
     logging.debug("Out plus:" + rgx)
 
     return rgx, nfa
 
-def elementary_RE(rgx, nfa):
+def elementary_RE(rgx, nfa, curr_state):
     logging.debug("In elementary:" + rgx)
     if len(rgx) > 0:
         if rgx[0] == "(":
-            rgx, nfa = group_RE(rgx, nfa)
+            rgx, nfa = group_RE(rgx, nfa, curr_state)
         elif rgx[0] == ".":
-            rgx, nfa = any_RE(rgx, nfa)
+            rgx, nfa = any_RE(rgx, nfa, curr_state)
         elif rgx[0] == "$":
-            rgx, nfa = eos_RE(rgx, nfa)
+            rgx, nfa = eos_RE(rgx, nfa, curr_state)
         elif rgx[0] == "[":
-            rgx, nfa = set_RE(rgx, nfa)
+            rgx, nfa = set_RE(rgx, nfa, curr_state)
         else:
-            rgx, nfa = char_RE(rgx, nfa)
+            rgx, nfa = char_RE(rgx, nfa, curr_state)
 
     logging.debug("Out elementary:" + rgx)
     return rgx, nfa
 
-def group_RE(rgx, nfa):
+def group_RE(rgx, nfa, curr_state):
     logging.debug("In group:" + rgx)
     if len(rgx) > 0:
         if rgx[0] == "(":
             rgx = next(rgx)
-            rgx, nfa = RE(rgx, nfa)
+            rgx, nfa = RE(rgx, nfa, curr_state)
             if rgx[0] == ")":
                 rgx = next(rgx)
             else:
                 logging.critical("Error in group parse. Missing ')'")
+                sys.exit(1)
         else:
             logging.critical("Error in group parse. Missing '('")
+            sys.exit(1)
 
     logging.debug("Out group:" + rgx)
     return rgx, nfa
 
-def any_RE(rgx, nfa):
+def any_RE(rgx, nfa, curr_state):
     logging.debug("In any:" + rgx)
     val_chars = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     for l in val_chars:
@@ -147,14 +165,14 @@ def any_RE(rgx, nfa):
     logging.debug("Out any:" + rgx)
     return rgx, nfa
 
-def eos_RE(rgx, nfa):
+def eos_RE(rgx, nfa, curr_state):
     logging.debug("In eos:" + rgx)
     rgx = next(rgx)
 
     logging.debug("Out eos:" + rgx)
     return rgx, nfa
 
-def char_RE(rgx, nfa):
+def char_RE(rgx, nfa, curr_state):
     logging.debug("In char:" + rgx)
     char_patt = re.compile("[a-zA-Z0-9]")
     if len(rgx) > 0:
@@ -163,30 +181,31 @@ def char_RE(rgx, nfa):
             rgx = next(rgx)
         else:
             logging.critical("Error in character parse. Invalid character")
+            sys.exit(1)
 
     logging.debug("Out char:" + rgx)
 
     return rgx, nfa
 
-def set_RE(rgx, nfa):
+def set_RE(rgx, nfa, curr_state):
     logging.debug("In set:" + rgx)
     if len(rgx) > 0:
         if rgx[0] == "[":
             if rgx[1] == "^":
-                rgx, nfa = neg_set_RE(rgx, nfa)
+                rgx, nfa = neg_set_RE(rgx, nfa, curr_state)
             else:
-                rgx, nfa = pos_set_RE(rgx, nfa)
+                rgx, nfa = pos_set_RE(rgx, nfa, curr_state)
 
     logging.debug("Out set:" + rgx)
 
     return rgx, nfa
 
-def pos_set_RE(rgx, nfa):
+def pos_set_RE(rgx, nfa, curr_state):
     logging.debug("In pos_set:" + rgx)
     if len(rgx) > 0:
         if rgx[0] == "[":
             rgx = next(rgx)
-            rgx, nfa = set_items_RE(rgx, nfa)
+            rgx, nfa = set_items_RE(rgx, nfa, curr_state)
             if rgx[0] == "]":
                 rgx = next(rgx)
 
@@ -194,14 +213,15 @@ def pos_set_RE(rgx, nfa):
 
     return rgx, nfa
 
-def neg_set_RE(rgx, nfa):
+# IF I have [^a], is a part of the alphabet?
+def neg_set_RE(rgx, nfa, curr_state):
     logging.debug("In neg_set:" + rgx)
     if len(rgx) > 0:
         if rgx[0] == "[":
             rgx = next(rgx)
             if rgx[0] == "^":
                 rgx = next(rgx)
-                rgx, nfa = set_items_RE(rgx, nfa)
+                rgx, nfa = set_items_RE(rgx, nfa, curr_state)
                 if rgx[0] == "]":
                     rgx = next(rgx)
 
@@ -210,20 +230,20 @@ def neg_set_RE(rgx, nfa):
     return rgx, nfa
 
 # ------ Needs fixing --------
-def set_items_RE(rgx, nfa):
+def set_items_RE(rgx, nfa, curr_state):
     logging.debug("In set_items:" + rgx)
-    rgx, nfa = set_item_RE(rgx, nfa)
+    rgx, nfa = set_item_RE(rgx, nfa, curr_state)
 
-    rgx, nfa = set_item_RE(rgx, nfa)
-    rgx, nfa = set_items_RE(rgx, nfa)
+    rgx, nfa = set_item_RE(rgx, nfa, curr_state)
+    rgx, nfa = set_items_RE(rgx, nfa, curr_state)
 
     logging.debug("Out set_items:" + rgx)
 
     return rgx, nfa
 
-def set_item_RE(rgx, nfa):
+def set_item_RE(rgx, nfa, curr_state):
     logging.debug("In set_item:" + rgx)
-    rgx, nfa = range_RE(rgx, nfa)
+    rgx, nfa = range_RE(rgx, nfa, curr_state)
     #rgx, nfa = char_RE(rgx, nfa)
 
     logging.debug("Out set_item:" + rgx)
@@ -231,26 +251,27 @@ def set_item_RE(rgx, nfa):
 
 # Looks at char
 # checks if range or just char
-def range_RE(rgx, nfa):
+def range_RE(rgx, nfa, curr_state):
     logging.debug("In range:" + rgx)
-    rgx, nfa = char_RE(rgx, nfa)
+    rgx, nfa = char_RE(rgx, nfa, curr_state)
     if len(rgx) > 0:
         if rgx[0] == "-":
             rgx = next(rgx)
-            rgx, nfa = char_RE(rgx, nfa)
+            rgx, nfa = char_RE(rgx, nfa, curr_state)
 
     logging.debug("Out range:" + rgx)
 
     return rgx, nfa
 
 # Regex parser
-def RE(rgx, nfa):
+def RE(rgx, nfa, curr_state):
     logging.debug("In RE:" + rgx)
-    rgx, nfa = simple_RE(rgx, nfa)
-    if re.match('^[a-zA-Z0-9]*$', rgx):
-        rgx, nfa = simple_RE(rgx, nfa)
+    #rgx, nfa = simple_RE(rgx, nfa, curr_state)
+    #if re.match('^[a-zA-Z0-9]*$', rgx):
+    if '|' in  rgx:
+        rgx, nfa = union_RE(rgx, nfa, curr_state)
     else:
-        rgx, nfa = union_RE(rgx, nfa)
+        rgx, nfa = simple_RE(rgx, nfa, curr_state)
 
     logging.debug("Out RE:" + rgx)
     return rgx, nfa
