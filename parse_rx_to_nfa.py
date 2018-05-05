41d19435dc70ba4cdedafae5da0cae8e1317aaf1
Jesse-Opitz <jesse.opitz1@marist.edu>
2018-05-05 19:39:57 -0400

    Parses characters in square braces to NFA

--- parse_rx_to_nfa.py
+++ parse_rx_to_nfa.py
@@ -15,6 +15,7 @@ def parse_RE(rx, nfa, p, curr_state):
             parse_RE(x, nfa, 0, 0)
     else:
         while p < len(rx):
+            print rx[p]
             if re.match("[a-zA-Z0-9]", rx[p]):
                 # If it's a character
                 if (p+1) < len(rx):
@@ -23,14 +24,68 @@ def parse_RE(rx, nfa, p, curr_state):
                         nfa, p, curr_state = parse_char(rx, nfa, p, curr_state)
                     elif rx[p+1] == '*':
                         nfa, p, curr_state = parse_star(rx, nfa, p, curr_state)
+                        # Jump letter and star/plus
+                        p = p + 2
                     elif rx[p+1] == '+':
                         nfa, p, curr_state = parse_plus(rx, nfa, p, curr_state)
+                        # Jump letter and star/plus
+                        p = p + 2
                 else:
                     # This is fine, doesn't have to check for * or + when its last character
                     nfa, p, curr_state = parse_char(rx, nfa, p, curr_state)
             elif rx[p] == '[':
                 # If it's a set
-                print "["
+
+                p += 1
+
+                char_list = ''
+                
+                while not (rx[p] == ']'):
+                    char_list += rx[p]
+                    p += 1
+
+                next_state = nfa.get_next_state()
+
+                if p+1 < len(rx):
+                    if rx[p+1] == '*':
+                        # 0 or more
+                        print 'star'
+                    elif rx[p+1] == '+':
+                        #1 or more
+                        print 'plus'
+                    else:
+                        if '-' in char_list:
+                            # Range
+                            print 'range'
+                        else:
+                            # Just characters
+                            nfa.add_state('q' + str(nfa.get_next_state()), False, False)
+
+                            for char in char_list:
+                                # Add to alphabet and create transition for each character
+                                nfa.add_to_ab(char)
+                                nfa.add_transition(char, 'q' + str(curr_state), 'q' + str(next_state))
+
+                            curr_state = next_state
+
+                            p += 1
+		else:
+		    if '-' in char_list:
+                        # Range
+                        print 'range'
+                    else:
+                        # Just characters
+                        nfa.add_state('q' + str(nfa.get_next_state()), False, False)
+
+                        for char in char_list:
+                            # Add to alphabet and create transition for each character
+                            nfa.add_to_ab(char)
+                            nfa.add_transition(char, 'q' + str(curr_state), 'q' + str(next_state))
+                        
+                        curr_state = next_state
+
+                        p += 1
+
             elif rx[p] == '(':
                 # If it's a group
                 print "("
@@ -90,11 +145,6 @@ def parse_star(rx, nfa, p, curr_state):
     nfa.add_transition('Eps', 'q' + str(curr_state), 'q' + str(state_before_char))
     nfa.add_transition('Eps', 'q' + str(start_state), 'q' + str(curr_state))
     
-    # Jump letter and star/plus
-    p = p + 2
-
-    #nfa, p, curr_state = loop(rx, nfa, p, curr_state)    
-
     logging.debug("Out star:" + str(p) + " curr_state:" + str(curr_state))
 
     return nfa, p, curr_state
@@ -128,9 +178,6 @@ def parse_plus(rx, nfa, p, curr_state):
 
     # loopback epsilon transitions
     nfa.add_transition('Eps', 'q' + str(curr_state), 'q' + str(state_before_char))
-
-    # Jump letter and star/plus
-    p = p + 2    
 
     logging.debug("Out plus:" + str(p) + " curr_state:" + str(curr_state))
 
